ca7d5d2e46b97fc4f70c889ce7f0b93a2dd95169
palatka22 <62890199+palatka22@users.noreply.github.com>
2021-03-12 18:10:24 +0100

    ketto

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-$("#fox1").text('Macrotis');
-$("#fox2").css('border-color', 'purple');
+$("#fox1").text('Macrotis')
+$("#fox2").css('border-color', 'purple')
 $("p").css('background-color','yellow')
 
-let szin='pink';
-if (szin='pink'){
+let szin ="pink"
+if (szin =="pink"){
+    $('body').append(
+        '<h1>Baszod ez lefut</h1>'
+      );
     $("#fox3").css('background-color', 'pink');
 }
 let szam=101
@@ -18,8 +21,14 @@ $('body').append(
     '<h1>Hello World!</h1>'
   );
 
+
   let myArray = ['Johny Filpo', 'Claire', 'Mark', 'Susan'];
-  myArray.forEach(element => {
-    $('body').append(
-        element      );
-  });
+  myArray.forEach(elem => {
+    
+if (elem== "Mark"){
+    $('ul').append('<li><strong>'+elem+' hopp</strong></li>' )}  
+else {
+    $('ul').append('<li>'+elem+' hopp</li>' )
+}
+});
+  
